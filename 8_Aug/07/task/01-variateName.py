3e18f65561f7cdc2f064369adc3662263d6269a2
Fizz_kai_mac <727354123@qq.com>
2017-08-07 17:36:31 +0800

    init

---
+++ 8_Aug/07/task/01-variateName.py
@@ -0,0 +1,10 @@
+print('ce shi bian liang de ming cheng!!')
+temp = '李四'
+Temp = '张三'
+tEMp = '佚名'
+temP = '王五'
+temp = 911
+print('temp = ' + str(temp))
+print('Temp = ' + Temp)
+print('tEMp = ' + tEMp)
+print('temP = ' + temP)
