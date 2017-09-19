03176a0c43522c4de81121ccaaae163b48433737
Fizzkai_win7 <727354123@qq.com>
2017-09-19 17:19:18 +0800

    交接工作

--- 8_Aug/07/01-firstGame.py
+++ 8_Aug/07/01-firstGame.py
@@ -2,9 +2,9 @@ print('---------Python第一个测试----------')
 temp = input('不妨猜一下我现在心里想的是哪个英文:')
 guess = temp
 if guess == 'MMP':
-	print('我曹，你是我心里的蛔虫么')
-	print('哼！猜中了也没有奖励')
+    print('我曹，你是我心里的蛔虫么')
+    print('哼！猜中了也没有奖励')
 else:
-	print('猜错了，我心里想的是MMP')
+    print('猜错了，我心里想的是MMP')
 print('游戏结束，不玩啦！！！')
 input('\n------over--------\n如果是cmd，按回车关闭窗口！')
